991323166c1ccdb9d4e6b47d5c2b63e9955c9c04
williampmb <williampmb@gmail.com>
2017-05-13 19:00:20 -0300

    Adding tickets view

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,5 +1,7 @@
 class TicketsController < ApplicationController
   def index
-  	 @tickets = Ticket.all
+  	id_user = 2
+  	@tickets = Ticket.where(user_id: id_user).order(:created_at)
+  	
   end
 end
