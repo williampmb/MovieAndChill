4badbe1513af6e853ed0f71b83eb048c5c798a08
williampmb <williampmb@gmail.com>
2017-06-24 12:34:50 -0300

    Fixed layout Movie, add forms, not complety yet

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
 	has_many :movie_theater
 	has_many :sessions
-	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/assets/green-background.png"
+	has_attached_file :image, styles: { medium: "300x300>", thumb: "75x100#" }, default_url: "/assets/missing75x100.jpg"
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
