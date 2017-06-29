4dcbea3a7674bcb9a3545aacb18ca158880b859a
Raphael Pinheiro de Souza <raphael.pinheiro.souza@gmail.com>
2017-06-28 22:17:01 -0300

    Added User’s edit and delete pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,12 +34,35 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
+	def index
+		@users = User.all
+	end
+
 	def create
 		@user = User.new(user_params)
 		if @user.save
 			flash[:success] = "User added"
 			redirect_to root_path
 		end
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+	    if @user.update_attributes(user_params)
+	    	flash[:success] = "User edited"
+	    	redirect_to users_path
+	    end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		flash[:success] = "User deleted"
+		redirect_to users_path
 	end
 
 	def buy_ticket
